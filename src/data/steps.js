47280309557942f7985aa7953ec572917be1f8af
Nathan Griffiths <ngriffiths@postmedia.com>
2025-06-05 08:22:49 -0700

    typo

--- src/data/steps.js
+++ src/data/steps.js
@@ -2,6 +2,6 @@ export default [
     'Each of the 100 highest-paid public sector employees earned nearly $500,000 in 2023/24. <span class="scroll-more">Scroll for more...</span>',
     '<span class="bright-purple-01">Only 26 were women</span>',
     'Three organizations employed 87 of B.C.’s highest earners: UBC, Powerex and the Provincial Health Services Authority. <br /><br/><span class="bright-purple-01">Only 24 per cent were women</span>',
-    'Four people earned over $1 million in 2023/24. <br/><br /><span class="bright-purple-01">Only one was a womn</span>',
+    'Four people earned over $1 million in 2023/24. <br/><br /><span class="bright-purple-01">Only one was a woman</span>',
     'Women in the top 100 were most likely to reach top-paid positions at the Provincial Health Services Authority, part of a sector where women are over represented. Roughly three-quarters of B.C. health-care workers earning $75,000 or more are women, according to Postmedia’s data.<br/><br/><span class="bright-purple-01">79 per cent of women in the top 100 worked at the PHSA</span>'
 ];
